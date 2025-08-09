18d894736df40a04c864d6f631a1182cfdd6e5fd
Dimasiqqqq <142820357+Dimasiqqqq@users.noreply.github.com>
2025-08-09 13:34:19 +0300

    hw-2b

---
+++ src/server.js
@@ -0,0 +1,75 @@
+import cors from 'cors';
+import express from 'express';
+import pino from 'pino-http';
+
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+const PORT = getEnvVar('PORT', 3000);
+
+
+
+
+export const startServer = () => { 
+    const app = express();
+    app.use(express.json());
+    app.use(cors());
+
+    app.use(
+        pino({
+          transport: {
+          target: 'pino-pretty',
+          },
+        }),
+    );
+
+    
+    app.get('/', (req, res) => {
+        res.status(200).json({ message: 'Welcome to MongoDB test' });
+    });
+
+
+
+    app.get('/contacts', async (req, res) => {
+        const contact = await getAllContacts();
+        res.json({
+            status: 200,
+            message: 'Successfully found contacts!',
+            data: contact,
+        });
+    });
+
+    app.get('/contacts/:contactId', async (req, res, next) => {
+        const { contactId } = req.params;
+        
+        try {
+          const contact = await getContactById(contactId);
+    
+          if (!contact) {
+            return res.status(404).json({
+              message: 'Contact not found',
+            });
+          }
+    
+          res.json({
+            status: 200,
+            message: `Successfully found contact with id ${contactId}!`,
+            data: contact,
+          });
+        } catch (error) {
+          next(error);
+        }
+      });
+
+
+    
+    app.use('*', (req, res) => {
+        res.status(404).json({
+        message: 'Not found',
+        });
+    });
+
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
